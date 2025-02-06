ae2218e203db9eb100c87d37251f914943c8c01a
ThaN1eL <yuiharabusiness@gmail.com>
2025-02-06 20:53:41 +0800

    clone temp

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true,
-  });
+// Toggle class active hamburger menu
+
+const navbarNav = document.querySelector(' .navbar');
+
+// ketika humburger menu di click
+document.querySelector('#hamburger-menu').
+onclick = (e) => {
+navbarNav.classList.toggle('navbar-active');
+e.preventDefault();
+};
+
+
+// klik di luar sidebar untuk menghilangkan nav dan search box
+
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('navbar-active');
+    }
+    
+});
